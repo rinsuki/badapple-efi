979e283ccbe110da868f182c7f521ace7ecde327
rinsuki <428rinsuki+git@gmail.com>
2021-04-23 01:58:59 +0900

    feat: select resolutions for Bad Apple!!

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,44 @@ use uefi::ResultExt;
 fn efi_main(handle: Handle, system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&system_table).expect_success("Failed to initialize");
     info!("Hello, world!");
-    if let Ok(mut gopp) = system_table.boot_services().locate_protocol::<GraphicsOutput>() {
-        let gop = unsafe { &mut *gopp.unwrap().get() };
+    init_gop(system_table.boot_services());
+    Status::SUCCESS
+}
+
+fn init_gop(boot_services: &BootServices) {
+    if let Ok(gopp) = boot_services.locate_protocol::<GraphicsOutput>() {
+        let mut gop = unsafe { gopp.unwrap().get().as_mut() }.unwrap();
+        info!("Show Available Resolutions:");
+        let mut usedMode = None;
+        let mut score = i32::MIN;
         for mode in gop.modes() {
             let inf = mode.unwrap();
             let inff = inf.info();
             let (width, height) = inff.resolution();
-            info!("Mode: {}, {}", width, height);
+            let currentScore = if width == 512 && height == 384 {
+                i32::MAX
+            } else if (width % 512 == 0 && height % 384 == 0) && ((width / 512) == (height / 384)) {
+                i32::MAX - (width/512) as i32
+            } else if (width % 4 == 0 && height % 3 == 0) && ((width / 4) == (width / 3)) {
+                (i32::MAX / 2) - (width/4) as i32
+            } else {
+                -((width * height) as i32)
+            };
+            info!("{} x {} (Score: {})", width, height, currentScore);
+            if width < 512 || height < 384 {
+                continue;
+            }
+            if score < currentScore {
+                score = currentScore;
+                usedMode = Some(inf);
+            }
         }
+        if let Some(mode) = usedMode {
+            let (width, height) = mode.info().resolution();
+            info!("Choiced: {} x {} (Score: {})", width, height, score);
+            gop.set_mode(&mode).expect("failed to set mode").expect("failed to set mode");
+        }
+        return;
     }
-    Status::SUCCESS
+    panic!("Failed to Initialize GOP (or unavailable)");
 }
